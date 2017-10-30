96657c9b4ded2c281e045aa85d13bb94cda60fe6
Debashish Nayak <debashishzero@gmail.com>
2017-10-30 20:26:34 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello! Expensify here.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.css';
+
+//import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
