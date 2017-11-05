4e8ba873710925233ba6027c34f3a1d043a8519c
Debashish Nayak <debashishzero@gmail.com>
2017-11-05 15:22:30 +0530

    Added filters reducers

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -8,6 +8,21 @@ const expensesReducer = (state = defaultState, action) => {
         ...state,
         action.expense
       ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => (id !== action.id));
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return [
+            ...expense,
+            ...action.updates
+          ];
+        } else {
+          return expense;
+        }
+      });
+    default:
+      return state;
   }
 };
 
---
+++ src/reducers/filters.js
@@ -0,0 +1,41 @@
+const defaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+// state = state.filters
+const filtersReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      };
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      };
+    default:
+      return state;
+  }
+};
+
+export default filtersReducer;
