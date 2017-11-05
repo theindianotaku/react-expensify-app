dddeccb4841e6d7426c91176c6f18276b5e49e5f
Debashish Nayak <debashishzero@gmail.com>
2017-11-05 18:27:59 +0530

    Added sorting

---
+++ src/selectors/expenses.js
@@ -0,0 +1,24 @@
+const setVisibleExpenses = (
+  expenses,
+  { text, sortBy, startDate, endDate}
+) => {
+  const filteredExpense = expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  });
+
+  const sortedExpense = filteredExpense.sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+
+  return sortedExpense;
+};
+
+export default setVisibleExpenses;
