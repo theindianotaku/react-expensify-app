2c02bd0d3b8058cf5cc3f2d94a51c5f3826b69a3
Debashish Nayak <debashishzero@gmail.com>
2017-12-29 10:40:54 +0530

    Added Redux Dev Tools support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,9 +6,10 @@ import filtersReducer from '../reducers/filters';
 export default () => {
   const store = createStore(
     combineReducers({
-      expenses: expensesReducer ,
+      expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
