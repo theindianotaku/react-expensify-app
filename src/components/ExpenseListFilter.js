3870728cf3ebb5b760ad419563bf57055a8f2254
Debashish Nayak <debashishzero@gmail.com>
2017-12-29 21:09:04 +0530

    Wired up jest testing

--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
