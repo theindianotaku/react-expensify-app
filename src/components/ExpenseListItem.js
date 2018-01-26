efe8ac823811e8a974c09631654a253485af4a40
Debashish Nayak <debashishzero@gmail.com>
2018-01-26 22:37:47 +0530

    Updated dependancies, added numeral and updated snapshots

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, amount, createdAt, description }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('Do MMMM, YYYY')}
+    </p>
   </div>
 );
 
