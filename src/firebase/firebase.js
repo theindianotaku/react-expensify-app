c00f72e834e0ebe092b6067062935189285d76da
Debashish Nayak <debashishzero@gmail.com>
2018-02-02 14:57:47 +0530

    Setup test and dev  REACT_APP env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: 'AIzaSyBTruV31XROZ7RhvVpF7aH0156WiRuAzbc',
-  authDomain: 'expensify-de6bd.firebaseapp.com',
-  databaseURL: 'https://expensify-de6bd.firebaseio.com',
-  projectId: 'expensify-de6bd',
-  storageBucket: 'expensify-de6bd.appspot.com',
-  messagingSenderId: '1009495622037'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
