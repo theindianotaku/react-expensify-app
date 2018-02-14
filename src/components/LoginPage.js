d857501180e671cdb3263726d4f4813f0f8d36b1
Debashish Nayak <debashishzero@gmail.com>
2018-02-14 12:14:24 +0530

    Style buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../thunks/auth';
 
+import GoogleLogo from '../images/google-logo.png';
+
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expense-ify</h1>
       <p>It's time to get your expenses under control.</p>
-      <button onClick={startLogin}>Login</button>
+      <button className="signin-button valign-wrapper center-align" onClick={startLogin}>
+        <span className="signin-button__icon valign-wrapper">
+          <img alt="google-logo" src={GoogleLogo} />
+        </span>
+        <span className="signin-button__content">
+          Login with Google
+        </span>
+      </button>
     </div>
   </div>
 );
